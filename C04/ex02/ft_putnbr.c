781a0971ce4a76fbabfc39e01293b2150ab106b4
Gaëtan Tokotuu <gaetan.tokotuu@gmail.com>
2019-10-12 14:40:48 +0200

    C04 - ex02

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2", 2);
+		ft_putnbr(147483648);
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
