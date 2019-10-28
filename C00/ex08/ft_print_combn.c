2c37c5488e7dd8ac2e718ea2bcb684cc0702ba96
Gaëtan Tokotuu <gaetan.tokotuu@gmail.com>
2019-10-28 19:19:24 +0100

    C00 - ex08 vfinal

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,40 +1,73 @@
 #include <unistd.h>
-#include <stdio.h>
-
-int		ft_calcpow(int n)
-{
-	int pow;
-
-	pow = 1;
-	while (n > 0)
-	{	
-		pow = pow * 10;
-		n--;
-	}
-	return (pow);
-}
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_print_combn(int n)
+int		ft_print_tab(int *tab, int size)
 {
-	int nb;
-	int pow;
+	int i;
+	int j;
+	int cres;
 
-	nb = 1;
-	pow = ft_calcpow(n);
-	while (nb < pow)
-	{	
-		if (nb >)
-		write(1, ", ", 2);
+	i = 1;
+	j = 0;
+	cres = 0;
+	while (i < size)
+	{
+		if (tab[i - 1] >= tab[i])
+			cres = 1;
+		i++;
+	}
+	if (cres == 0)
+	{
+		while (j < size)
+		{
+			ft_putchar(tab[j] + 48);
+			j++;
+		}
+	}
+	return (cres);
+}
+
+void	ft_init_tab(int *tab, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		tab[i] = i;
+		i++;
 	}
 }
 
-int		main(void)
+void	ft_print_combn(int n)
 {
-	ft_print_combn(3);
-	return (0);
+	int i;
+	int tab[n];
+
+	i = 0;
+	if (n < 1 || n > 9)
+		return ;
+	ft_init_tab(tab, n);
+	while (tab[0] <= 10 - n)
+	{
+		if (ft_print_tab(tab, n) == 0)
+			if (tab[0] != 10 - n)
+				write(1, ", ", 2);
+		while (i < n)
+		{
+			if (tab[i] >= 9 && n > 1)
+			{
+				tab[i] = 0;
+				tab[i - 1]++;
+			}
+			if (i == n - 1)
+				tab[i]++;
+			i++;
+		}
+		i = 0;
+	}
 }
