ea9c1f446c5109ca9a4d7943b79f2aa5ab66685c
Gaëtan Tokotuu <gaetan.tokotuu@gmail.com>
2019-11-04 22:59:00 +0100

    C05 - ex02

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,16 @@
+int		ft_iterative_power(int nb, int power)
+{
+	int i;
+	int res;
+
+	i = 0;
+	res = 1;
+	if (power < 0)
+		return (0);
+	while (i < power)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
+}
